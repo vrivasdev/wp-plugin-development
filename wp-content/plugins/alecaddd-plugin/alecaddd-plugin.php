e76b802d368fe6ba6290b870dfb1410160413dd5
Victor Rivas <vrivas@teravisiontech.com>
2018-09-26 16:58:11 -0400

    Part 9 - Settings Link and Admin Pages

--- wp-content/plugins/alecaddd-plugin/alecaddd-plugin.php
+++ wp-content/plugins/alecaddd-plugin/alecaddd-plugin.php
@@ -51,24 +51,45 @@ if ( !class_exists('AlecadddPlugin') ) {
 
 	class AlecadddPlugin 
 	{
+
+		public $plugin; // plugin file name
+
+		function __construct() {
+			$this->plugin = plugin_basename( __FILE__ );			
+		}
+
 		function register() {
-			// add_action( 'admin_enqueue_scripts', [ $this, 'enqueue'] ); // Object initialization
-			add_action( 'admin_enqueue_scripts', [ 'AlecadddPlugin', 'enqueue'] ); // Static method way
+			//add_action( 'admin_enqueue_scripts', [ 'AlecadddPlugin', 'enqueue'] ); // Static method way
+			add_action( 'admin_enqueue_scripts', [ $this, 'enqueue'] ); // Object initialization
+			add_action( 'admin_menu', [ $this, 'add_admin_pages'] );			
+
+			add_filter( "plugin_action_links_$this->plugin", [ $this, 'settings_link' ] ); // admin links filter
+		}
+
+		public function settings_link( $links ) {			
+			// add custom settings link			
+			array_push( $links, '<a href="admin.php?page=alecaddd-plugin">Settings</a>'); // append to links native array
+			return $links;
+		}
+
+		public function add_admin_pages() {
+			add_menu_page( 'Alecaddd PLugin', 'Alecaddd Plugin', 'manage_options', 'alecaddd-plugin', [ $this, 'admin_index'], 'dashicons-store', 110 );
+		}
+
+		public function admin_index() {
+			// require template
+			require_once plugin_dir_path( __FILE__ ) . 'templates/admin.php';			
+
 		}
 
 		protected function create_post_type() {
 			add_action( 'init', [ $this, 'custom_post_type' ] );
 		}
 
-		function activate() {
-			
+		function activate() {			
 			//$this->custom_post_type(); // Create CPT
 			require_once plugin_dir_path( __FILE__ ) . 'inc/alecaddd-plugin-activate.php';			
 			AlecadddPluginActivate::activate();			
-		}
-
-		function deactivate() {
-			// flush the rewrite rules
 		}
 
 		function custom_post_type() {
@@ -84,7 +105,7 @@ if ( !class_exists('AlecadddPlugin') ) {
 
 	$alecadddPlugin = new AlecadddPlugin();
 	$alecadddPlugin->register();
-	// AlecadddPlugin::register();
+	// AlecadddPlugin::register(); // static way
 }
 
 // activation
